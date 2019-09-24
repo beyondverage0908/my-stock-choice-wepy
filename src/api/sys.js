1655c913e841259b8028b62677abb7146098e7a7
beyondverage0908 <beyondaverage0908@gmail.com>
2019-09-24 17:21:49 +0800

    修改

--- src/api/sys.js
+++ src/api/sys.js
@@ -1,6 +1,6 @@
 import { get, post } from './http';
 
 // 获取个股票信息
-export const sysVisit = () => {
-	return post(`https://m.00315.com/wxapp/api/sys/visit`);
+export const sysVisit = (stockCode) => {
+	return post(`https://m.00315.com/wxapp/api/sys/visit/${stockCode}`);
 }
