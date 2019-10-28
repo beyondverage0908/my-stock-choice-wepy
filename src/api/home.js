064a6d08160d3fa5135da50503d389543dfa26be
beyondverage0908 <beyondaverage0908@gmail.com>
2019-10-28 09:19:07 +0800

    最近一次修改

--- src/api/home.js
+++ src/api/home.js
@@ -2,32 +2,32 @@ import { get, post, del } from './http';
 
 // 获取默认个股票信息
 export const getStockDefault = () => {
-	return get(`https://m.00315.com/wxapp/api/v2/stocks/default`);
+	return get(`/stocks/default`);
 }
 
 // 获取个股票信息
 export const getStockInfo = (stockCode = '') => {
-    return get(`https://m.00315.com/wxapp/api/v2/stocks/${stockCode}`);
+    return get(`/stocks/${stockCode}`);
 }
 
 // 搜索股票
 export const getSearchStockInfo = (searchText = '') => {
-    return get(`https://m.00315.com/wxapp/api/v2/stocks?q=${searchText}`);
+    return get(`/stocks?q=${searchText}`);
 }
 
 // 个股评级明细
 export const getRatings = (stockCode) => {
-    return get(`https://m.00315.com/wxapp/api/v2/stocks/${stockCode}/ratings`)
+    return get(`/stocks/${stockCode}/ratings`)
 }
 
 // 个股ROE评级明细
 export const getRoeratings = (stockCode) => {
-    return get(`https://m.00315.com/wxapp/api/v2/stocks/${stockCode}/roeratings`)
+    return get(`/stocks/${stockCode}/roeratings`)
 }
 
 // 个股利润评级明细
 export const getProfitRatings = (stockCode) => {
-    return get(`https://m.00315.com/wxapp/api/v2/stocks/${stockCode}/profitratings`)
+    return get(`/stocks/${stockCode}/profitratings`)
 }
 
 // 添加个股
@@ -36,17 +36,17 @@ export const addToStocks = ({ zqdm, zqjc }) => {
 		zqdm,
 		zqjc
 	}
-	return post(`https://m.00315.com/wxapp/api/v2/user/stocks`, data)
+	return post(`/user/stocks`, data)
 }
 
 // 移除个股
 export const removeFromStocks = (stockCode) => {
-	return del(`https://m.00315.com/wxapp/api/v2/user/stocks/${stockCode}`)
+	return del(`/user/stocks/${stockCode}`)
 }
 
 // 获取用户自选个股列表
 export const getUserStocks = () => {
-	return get(`https://m.00315.com/wxapp/api/v2/user/stocks`);
+	return get(`/user/stocks`);
 }
 
 
